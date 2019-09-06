2262771980a87336f435066a1059078478d7775f
Linus Alpsten <linus.alpsten@telia.com>
2019-09-06 13:21:42 +0200

    testing stuff

--- ConsoleSnake/ConsoleSnake/Program.cs
+++ ConsoleSnake/ConsoleSnake/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleSnake
@@ -10,13 +11,44 @@ namespace ConsoleSnake
     {
         static void Main(string[] args)
         {
-            List<List<IDrawable>> map = new List<List<IDrawable>>();
-            map.Add(new List<IDrawable> { new Tile('#'), new Tile('#'), new Tile('#'), new Tile('#'), new Tile('#') });
-            map.Add(new List<IDrawable> { new Tile('#'), new Tile('*'), new Tile('*'), new Tile('*'), new Tile('#') });
-            map.Add(new List<IDrawable> { new Tile('#'), new Tile('*'), new Tile('*'), new Tile('*'), new Tile('#') });
-            map.Add(new List<IDrawable> { new Tile('#'), new Tile('*'), new Tile('*'), new Tile('*'), new Tile('#') });
-            map.Add(new List<IDrawable> { new Tile('#'), new Tile('#'), new Tile('#'), new Tile('#'), new Tile('#') });
-            map.Draw();
+            //List<List<IDrawable>> map = new List<List<IDrawable>>();
+            //map.Add(new List<IDrawable> { new Tile('#'), new Tile('#'), new Tile('#'), new Tile('#'), new Tile('#') });
+            //map.Add(new List<IDrawable> { new Tile('#'), new Tile('*'), new Tile('*'), new Tile('*'), new Tile('#') });
+            //map.Add(new List<IDrawable> { new Tile('#'), new Tile('*'), new Tile('*'), new Tile('*'), new Tile('#') });
+            //map.Add(new List<IDrawable> { new Tile('#'), new Tile('*'), new Tile('*'), new Tile('*'), new Tile('#') });
+            //map.Add(new List<IDrawable> { new Tile('#'), new Tile('#'), new Tile('#'), new Tile('#'), new Tile('#') });
+            //map.Draw();
+            Map map = new Map(10, 10);
+            ConsoleKey keyPressed = ConsoleKey.Spacebar;
+            bool quit = false;
+            ManualResetEvent mre = new ManualResetEvent(false);
+
+            Task drawer = Task.Factory.StartNew(() =>
+            {
+                while (!quit)
+                {
+                    map.Draw();
+                    Console.WriteLine(keyPressed);
+                    Thread.Sleep(500);
+                    Console.Clear();
+                }
+            });
+
+            Task keyListener = Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    keyPressed = Console.ReadKey().Key;
+                    if (keyPressed == ConsoleKey.Q)
+                    {
+                        quit = true;
+                    }
+                }
+            });
+            while (!quit)
+            {
+
+            }
         }
     }
 }
